fbe3efeadfb9afbab79021171161e8f56090257a
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-11-08 14:47:54 -0300

    enviando todas as mensagens na porta messages, e porta opcional limit criada

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import express from "express";
 import cors from "cors";
 
-const app = express()
+const app = express();
+app.use(cors())
+app.use(express.json())
 
-app.get("/live", (req, res) => {
-    res.send("Tudo funcionando perfeitamente")
+app.get("/", (req, res) => {
+    res.send("Porta principal")
+})
+
+app.get("/messages", (req, res) => {
+  const limit = req.query.limit;
+
+  if (!limit) {
+    return res.send("Todas as mensagens devem ser enviadas");
+  }
+
+  res.send(`Enviando ${limit} mensagens`);
+  //Envie apenas as mensagens que aquele usuario pode receber
 });
 
-app.listen(5000, (req, res) => console.log("Server running on port: 5000"));
+app.listen(5000, () => console.log("Server running on port: 5000"));
