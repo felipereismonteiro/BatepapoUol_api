89636d386c5f1b8d442e41b5b7e307b4dfc19986
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-11-09 08:47:30 -0300

    buscando somente o numero de mensagens mandado pelo query

--- src/app.js
+++ src/app.js
@@ -2,31 +2,28 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
- 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-dotenv.config()
+dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-mongoClient.connect().then(()=> {
-  db = mongoClient.db("messages")
-});
-
-app.get("/", (req, res) => {
-  res.send("Porta principal");
+mongoClient.connect().then(() => {
+  db = mongoClient.db("messages");
 });
 
 app.get("/messages", (req, res) => {
   const limit = req.query.limit;
+  const buscandoMensagens = db.collection("messages").find().toArray();
 
-  if (!limit) {
-    return res.send("Todas as mensagens devem ser enviadas");
-  }
+  buscandoMensagens.then((mensagens) => {
+    return res.send(mensagens.slice(0, limit ? limit : mensagens.length));
+  });
 
-  res.send(`Enviando ${limit} mensagens`);
+  // res.send(`Enviando ${limit} mensagens`);
   // Envie apenas as mensagens que aquele usuario pode receber
 });
 
