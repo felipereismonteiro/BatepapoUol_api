1cb05a9857bdcb681233dc9a8b35426e64fd3227
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-11-09 08:29:04 -0300

    backend conectado ao banco de dados

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 import express from "express";
 import cors from "cors";
-
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+ 
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(()=> {
+  db = mongoClient.db("messages")
+});
 
 app.get("/", (req, res) => {
-    res.send("Porta principal")
-})
+  res.send("Porta principal");
+});
 
 app.get("/messages", (req, res) => {
   const limit = req.query.limit;
@@ -17,7 +27,7 @@ app.get("/messages", (req, res) => {
   }
 
   res.send(`Enviando ${limit} mensagens`);
-  //Envie apenas as mensagens que aquele usuario pode receber
+  // Envie apenas as mensagens que aquele usuario pode receber
 });
 
 app.listen(5000, () => console.log("Server running on port: 5000"));
